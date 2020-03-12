cb4cb29fd2b7579b117f6883db933386b61db344
Miles Watson <milestwatson@icloud.com>
2020-03-12 20:31:02 +0000

    Add project files.

---
+++ UI/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+
+using Microsoft.Data.Sqlite;
+using System.Diagnostics;
+using System.IO;
+
+namespace UI
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+
+
+	public partial class MainWindow : Window
+	{
+
+        SqliteConnection connection;
+		bool created;
+
+		public MainWindow()
+		{
+
+			//SQLiteConnection.CreateFile("hello.db)
+			InitializeComponent();
+		}
+
+		private void showButton_Click(object sender, RoutedEventArgs e)
+		{
+			connection = new SqliteConnection("Data Source=hello.db");
+			if (!File.Exists("./hello.db")) File.Create("hello.db");
+			connection.Open();
+			connection.Close();
+
+		}
+	}
+}

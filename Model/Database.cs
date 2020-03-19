525c47b1f635c767186ab64158bcf17de2e907c5
Miles Watson <milestwatson@icloud.com>
2020-03-19 19:52:16 +0000

    Example

--- Model/Database.cs
+++ Model/Database.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.IO;
+using Microsoft.Data.Sqlite;
+using System.Diagnostics;
 
 namespace Model
 {
 	public class Database
 	{
 		
-		public Database()
+		public Database(string filename)
 		{
-			if (!File.Exists("./Data/database.sqlite")) File.Create("./Data/database.sqlite");
+			if (!File.Exists(filename))
+			{
+				Directory.CreateDirectory(Path.GetDirectoryName(filename));
+				File.Create(filename);
+			}
+			
+			SqliteConnection conn = new SqliteConnection("Data Source=./Data/database.sqlite;Mode=ReadWriteCreate");
+			conn.Open();
+			SqliteCommand command = conn.CreateCommand();
+			command.CommandText = @"SELECT COUNT(name) FROM sqlite_master WHERE type = 'table' AND name = 'users';";
+			using (var reader = command.ExecuteReader())
+			{
+				reader.Read();
+				if (reader.GetInt32(0) == 0) {
+					Debug.WriteLine("Not created");
+				}
+			}
+			conn.Close();
+
 		}
+
+		
 
 	}
 }
--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using Microsoft.Data.Sqlite;
 using System.Diagnostics;
 using System.IO;
+using Model;
 
 namespace UI
 {
@@ -16,21 +17,18 @@ namespace UI
 	{
 
 		bool created;
+		Database db;
 
 		public MainWindow()
 		{
 
-			//SQLiteConnection.CreateFile("hello.db)
+			
 			InitializeComponent();
 		}
 
 		private void ShowButton_Click(object sender, RoutedEventArgs e)
 		{
-			SqliteConnection connection = new SqliteConnection("Data Source=hello.db");
-			if (!File.Exists("./hello.db")) File.Create("hello.db").Dispose();
-			connection.Open();
-			connection.Close();
-			connection.Dispose();
+			db = new Database("./Data/database.sqlite");
 		}
 	}
 }
